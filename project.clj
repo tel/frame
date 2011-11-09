aa08d863a6728975ba827d0d76f319df7f203a26
Joseph Abrahamson <abrahamson.j@gmail.com>
2011-11-08 20:37:26 -0500

    * project.clj: Removed dependency on core.match. It's kind of broken right now.

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
   :description "Nuts and bolts graphical object writing."
   :warn-on-reflection true
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/core.match "0.2.0-alpha6"]
                  [hiccup "0.3.7"]]
   :target-dir "target/"
   :repl-options [:init nil :caught clj-stacktrace.repl/pst+])
