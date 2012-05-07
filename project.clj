a2be49f01ed23d5acd0a72121bfefe9b13f3eb26
Joseph Abrahamson <abrahamson.j@gmail.com>
2012-05-06 23:52:52 -0400

    Beginning the rewrite.

--- project.clj
+++ project.clj
@@ -1,15 +1,10 @@
-(defproject frameworke/frame "0.0.1-SNAPSHOT"
-  :description "Nuts and bolts graphical object writing."
-  :warn-on-reflection true
-  :dev-dependencies [[com.googlecode.efficient-java-matrix-library/ejml "0.17"]
-                     [org.clojure/tools.cli "0.1.0"]
-                     [die-roboter "1.0.0-SNAPSHOT"]
-                     [cljfastdtw "1.0.4-SNAPSHOT"]
-                     [congomongo "0.1.7"]]
+(defproject framework/frame "0.0.1-SNAPSHOT"
+  :description "Layout engine for SVG in Hiccup; the nuts and bolts of Clojure SVG graphics."
+  :url "https://github.com/tel/frame"
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/algo.monads "0.1.0"]
-                 [slingshot "0.8.0"]
-                 [hiccup "0.3.7"]]
-  :target-dir "target/"
-  :repl-options [:init nil :caught clj-stacktrace.repl/pst+]
-  :clj-stacktrace {:test-color true})
+                 [slingshot "0.10.2"]
+                 [hiccup "1.0.0"]]
+  :profiles {:dev {:warn-on-reflection true
+                   :dependencies [[expectations "1.3.7"]]}})
